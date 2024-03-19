ccfdb5861044dc2d8a35a1abfbb1cc896a9cdadf
1231146378 <1231146378@FARBLAS18ACD07.acd.grupotiradentes.com>
2024-03-19 16:44:43 -0300

    tentando

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		if(2<3) {
-			System.out.println("Yuri é maluco");
+			System.out.println("Yuri é doido");
 		}
 	}
 
