d210847d22c1630b80cc05e8abab0921a84746ab
Yuri <anthony.yuri@souunit.com.br>
2024-03-19 16:55:05 -0300

    talvez não seja tão ruim

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		if(2<3) {
-			System.out.println("Yuri é doido");
-		}
+			if (2<3) {
+				System.out.println("Yuri é doido");
+			}
 	}
 
 }
